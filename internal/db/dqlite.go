fe01533952f380c0e92bc32fbb283ade33fcd3b2
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-08-28 10:47:58 +0200

    internal/db: Don't leave the request body open in case of error
    Add the response body to the reverter.
    Also closing the body should succeed. If not it can be considered an error.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- internal/db/dqlite.go
+++ internal/db/dqlite.go
@@ -453,10 +453,22 @@ func dqliteNetworkDial(ctx context.Context, addr string, db *DqliteDB) (net.Conn
 		return nil, fmt.Errorf("Failed to read response: %w", err)
 	}
 
-	defer response.Body.Close()
+	revert.Add(func() {
+		err := response.Body.Close()
+		if err != nil {
+			logCtx.Error("Failed to close dqlite response body", logger.Ctx{"error": err})
+		}
+	})
+
 	_, err = io.Copy(io.Discard, response.Body)
 	if err != nil {
 		logger.Error("Failed to read dqlite response body", logger.Ctx{"error": err})
+	}
+
+	// We are done reading the response body. Close it.
+	err = response.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to close dqlite response body: %w", err)
 	}
 
 	// If the remote server has detected that we are out of date, let's
